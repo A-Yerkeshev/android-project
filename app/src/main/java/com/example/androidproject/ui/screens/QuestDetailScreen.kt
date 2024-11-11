6c675b276681abcbb599a3139c351d246e9def5d
Artur Gol <artur.golavskiy@metropolia.fi>
2024-11-11 15:01:39 +0200

    1 error fix, conflicting imports hidden.

--- app/src/main/java/com/example/androidproject/ui/screens/QuestDetailScreen.kt
+++ app/src/main/java/com/example/androidproject/ui/screens/QuestDetailScreen.kt
@@ -17,7 +17,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ExpandMore
+//import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
