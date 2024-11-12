bf5cea4442d93f7550cc039174321c03f50353e7
dungdpham <dung.pham3@metropolia.fi>
2024-11-12 21:25:07 +0200

    make change to UI and styling

--- app/src/main/java/com/example/androidproject/ui/screens/QuestDetailScreen.kt
+++ app/src/main/java/com/example/androidproject/ui/screens/QuestDetailScreen.kt
@@ -7,24 +7,30 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
 import android.util.Log
+import androidx.compose.animation.core.animateSizeAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -41,13 +47,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.androidproject.R
 import com.example.androidproject.data.models.CheckpointEntity
@@ -218,17 +224,25 @@ fun QuestDetailScreen(
                         cameraState.zoom = 15.0
                     },
                     modifier = Modifier
+                        .size(76.dp)
                         .align(Alignment.BottomEnd)
                         .padding(16.dp),
-                    colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.secondary),
-                    shape = RoundedCornerShape(20.dp)
+                    colors = ButtonDefaults.buttonColors(MaterialTheme.colorScheme.primary),
+                    shape = CircleShape,
+                    contentPadding = PaddingValues(8.dp)
                 ) {
-                    Text(
-                        text = "Recenter",
-                        color = MaterialTheme.colorScheme.onSecondary,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        textAlign = TextAlign.Center
+//                    Text(
+//                        text = "Recenter",
+//                        color = MaterialTheme.colorScheme.onSecondary,
+//                        fontSize = 16.sp,
+//                        fontWeight = FontWeight.SemiBold,
+//                        textAlign = TextAlign.Center
+//                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_mylocation),
+                        contentDescription = "Center to my position",
+                        tint = MaterialTheme.colorScheme.onPrimary,
+                        modifier = Modifier.size(32.dp)
                     )
                 }
             }
