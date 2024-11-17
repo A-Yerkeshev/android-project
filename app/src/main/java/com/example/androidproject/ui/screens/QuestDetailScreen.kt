fcbb9db3c62a2e997614e0e78936235f481c3759
Anna Lindén <linden.a.anna@gmail.com>
2024-11-17 20:47:53 +0200

    checkpoint changes color

--- app/src/main/java/com/example/androidproject/ui/screens/QuestDetailScreen.kt
+++ app/src/main/java/com/example/androidproject/ui/screens/QuestDetailScreen.kt
@@ -8,18 +8,7 @@ import android.location.Location
 import android.location.LocationManager
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.CameraX
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.ImageProxy
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.LifecycleCameraController
-import androidx.camera.view.PreviewView
-import androidx.compose.animation.core.animateSizeAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -28,7 +17,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -40,7 +28,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -53,7 +40,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.livedata.observeAsState
@@ -66,14 +52,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.navigation.NavController
 import com.example.androidproject.R
 import com.example.androidproject.data.models.CheckpointEntity
 import com.example.androidproject.data.models.TaskEntity
@@ -84,8 +66,6 @@ import com.example.androidproject.ui.viewmodels.QuestViewModel
 import com.example.androidproject.ui.viewmodels.TaskViewModel
 import com.example.androidproject.utils.cameraPermission
 import com.example.androidproject.utils.locationPermission
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.common.util.concurrent.ListenableFuture
 import com.utsman.osmandcompose.CameraState
 import com.utsman.osmandcompose.Marker
 import com.utsman.osmandcompose.OpenStreetMap
@@ -93,8 +73,6 @@ import com.utsman.osmandcompose.rememberCameraState
 import com.utsman.osmandcompose.rememberMarkerState
 import org.osmdroid.config.Configuration
 import org.osmdroid.util.GeoPoint
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -373,7 +351,7 @@ fun QuestDetailScreen(
                                             shape = CircleShape,
                                             contentPadding = PaddingValues(4.dp)
                                         ) {
-                                            val icon = if (checkpoint.completed) R.drawable.baseline_check_circle_outline_24 else R.drawable.baseline_photo_camera_24
+                                            val icon = if (checkpoint.completed) R.drawable.ic_checkpoint_completed else R.drawable.baseline_photo_camera_24
                                             Icon(
                                                 painter = painterResource(id = icon),
                                                 contentDescription = "Take a photo",
@@ -453,13 +431,20 @@ fun ShowMap(
             }
 
             checkpoints.forEach { checkpoint ->
+
+                val iconResId = if (checkpoint.completed) {
+                    R.drawable.ic_checkpoint_completed
+                } else {
+                    R.drawable.ic_checkpoint_not_completed
+                }
+
                 Marker(
                     state = rememberMarkerState(
                         geoPoint = GeoPoint(checkpoint.lat, checkpoint.long)
                     ),
                     icon = ContextCompat.getDrawable(
                         context,
-                        R.drawable.baseline_radio_button_checked_24
+                        iconResId
                     ),
                     title = checkpoint.name,
                     onClick = {
